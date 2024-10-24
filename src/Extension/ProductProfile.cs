9bb878b63889524fe34f414f57f3477d4b122083
Joshua Ramos <auhsojsomar@gmail.com>
2024-10-25 01:58:58 +0800

    feat(api): :sparkles: Add `ProductProfile` for Auto Mapper

---
+++ src/Extension/ProductProfile.cs
@@ -0,0 +1,34 @@
+using AutoMapper;
+using The_Plague_Api.Data.Dto;
+using The_Plague_Api.Data.Entities;
+
+public class ProductProfile : Profile
+{
+  public ProductProfile()
+  {
+    // Product mappings
+    CreateMap<ProductDto, Product>()
+        .ForMember(dest => dest.Id, opt => opt.Ignore())
+        .ReverseMap();
+
+    // Discount mappings
+    CreateMap<DiscountDto, Discount>()
+        .ForMember(dest => dest.Id, opt => opt.Ignore())
+        .ReverseMap();
+
+    // Variant mappings
+    CreateMap<VariantDto, Variant>()
+        .ForMember(dest => dest.Id, opt => opt.Ignore())
+        .ReverseMap();
+
+    // Size mappings
+    CreateMap<SizeDto, Size>()
+        .ForMember(dest => dest.Id, opt => opt.Ignore())
+        .ReverseMap();
+
+    // Color mappings
+    CreateMap<ColorDto, Color>()
+        .ForMember(dest => dest.Id, opt => opt.Ignore())
+        .ReverseMap();
+  }
+}
