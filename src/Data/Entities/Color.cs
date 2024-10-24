7b1e8eddfeca13098c33a9466de0cee6b15e7340
Joshua Ramos <auhsojsomar@gmail.com>
2024-10-25 01:58:27 +0800

    fix(api): :rotating_light: fix warning for empty or not set

--- src/Data/Entities/Color.cs
+++ src/Data/Entities/Color.cs
@@ -10,10 +10,10 @@ namespace The_Plague_Api.Data.Entities
     [BsonRepresentation(BsonType.ObjectId)]
     public string? Id { get; set; } = ObjectId.GenerateNewId().ToString();
 
-    [Required, MaxLength(50)]
+    [MaxLength(50)]
     public required string Name { get; set; }
 
-    [Required, MaxLength(7)]
+    [MaxLength(7)]
     public required string HexCode { get; set; }
   }
 }
--- src/Data/Entities/Product.cs
+++ src/Data/Entities/Product.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 using The_Plague_Api.Data.Interface;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
 
 namespace The_Plague_Api.Data.Entities
 {
@@ -12,20 +13,22 @@ namespace The_Plague_Api.Data.Entities
     [BsonRepresentation(BsonType.ObjectId)]
     public string? Id { get; set; } = ObjectId.GenerateNewId().ToString();
 
+    [Required]
     [MaxLength(100)]
     public required string Name { get; set; }
 
     [MaxLength(500)]
-    public required string Description { get; set; }
+    public string? Description { get; set; }
 
-    public required string Image { get; set; }
+    public string? Image { get; set; }
 
+    [Required]
     [Column(TypeName = "decimal(18,2)")]
     public decimal Price { get; set; }
 
-    public Discount? Discount { get; set; }
+    public Discount? Discount { get; set; } // Optional relationship with a discount
 
-    public ICollection<Variant> Variants { get; set; } = new List<Variant>();
+    [Required]
+    public ICollection<Variant> Variants { get; set; } = new List<Variant>(); // Ensure initialization
   }
-
 }
