e8d656a01c7598c8b67eda50a3f93939861c0560
Joshua Ramos <auhsojsomar@gmail.com>
2024-10-25 02:13:45 +0800

    fix(api): :fire: Remove Id from `Color`, `Discount`, `Size`, `Variant` Dto

--- src/Data/Dto/ColorDto.cs
+++ src/Data/Dto/ColorDto.cs
@@ -2,7 +2,6 @@ namespace The_Plague_Api.Data.Dto
 {
   public class ColorDto
   {
-    public string? Id { get; set; }
     public string Name { get; set; } = String.Empty;
     public string HexCode { get; set; } = String.Empty;
   }
--- src/Data/Dto/SizeDto.cs
+++ src/Data/Dto/SizeDto.cs
@@ -2,7 +2,6 @@ namespace The_Plague_Api.Data.Dto
 {
   public class SizeDto
   {
-    public string? Id { get; set; }
     public string Name { get; set; } = String.Empty;
   }
 }
