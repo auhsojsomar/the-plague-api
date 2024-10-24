7043c6e85c819f44a6c30eecb998934a31598435
Joshua Ramos <auhsojsomar@gmail.com>
2024-10-25 01:50:44 +0800

    feat(api): :speech_balloon: Update `Discount Type Enum` to accept string or number

--- src/Data/Entities/DiscountType.cs
+++ src/Data/Entities/DiscountType.cs
@@ -1,8 +1,12 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+
 namespace The_Plague_Api.Data.Entities
 {
   public enum DiscountType
   {
-    Percent,
-    Cash
+    Percentage = 1,
+    FixedAmount = 2
   }
 }
