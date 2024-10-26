b530393d687da3dfc12156f6350ae4dffc496587
Joshua Ramos <auhsojsomar@gmail.com>
2024-10-26 11:37:52 +0800

    ci(api): :card_file_box: Add `Firstname`, `MiddleName`, and `LastName` to `User` Entity

--- src/Data/Entities/User.cs
+++ src/Data/Entities/User.cs
@@ -11,10 +11,20 @@ namespace The_Plague_Api.Data.Entities
     [BsonRepresentation(BsonType.ObjectId)]
     public string? Id { get; set; } = ObjectId.GenerateNewId().ToString();
 
-    [Required]
+    [Required(ErrorMessage = "First name is required.")]
+    public required string FirstName { get; set; }
+
+    public string? MiddleName { get; set; }
+
+    [Required(ErrorMessage = "Last name is required.")]
+    public required string LastName { get; set; }
+
+    [Required(ErrorMessage = "Email is required.")]
+    [EmailAddress(ErrorMessage = "Invalid email format.")]
     public required string Email { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "Password is required.")]
+    [MinLength(6, ErrorMessage = "Password must be at least 6 characters long.")]
     public required string Password { get; set; }
   }
 }
