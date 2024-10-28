08aac0df2c974888e64dafce0b932b86099e5e4f
Joshua Ramos <auhsojsomar@gmail.com>
2024-10-28 12:21:43 +0800

    fix(api): :card_file_box: Update the Image type from `string` to `Object` to support fetching and adding images.

--- src/Data/Dto/ProductDto.cs
+++ src/Data/Dto/ProductDto.cs
@@ -1,3 +1,5 @@
+using The_Plague_Api.Data.Entities.Product;
+
 namespace The_Plague_Api.Data.Dto
 {
   public class ProductDto
@@ -5,7 +7,7 @@ namespace The_Plague_Api.Data.Dto
     public string? Id { get; set; }
     public string Name { get; set; } = String.Empty;
     public string? Description { get; set; }
-    public string Image { get; set; } = String.Empty;
+    public Image? Image { get; set; }
     public decimal Price { get; set; }
     public DiscountDto? Discount { get; set; }
     public List<VariantDto> Variants { get; set; } = new List<VariantDto>();
---
+++ src/Data/Entities/Product/Image.cs
@@ -0,0 +1,8 @@
+namespace The_Plague_Api.Data.Entities.Product
+{
+  public class Image
+  {
+    public string? Main { get; set; }
+    public List<string>? Thumbnails { get; set; }
+  }
+}
