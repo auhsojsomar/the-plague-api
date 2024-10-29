47e2ade9f1b3afcfc95e5f814a7c67d3a133db51
Joshua Ramos <auhsojsomar@gmail.com>
2024-10-29 12:45:42 +0800

    ci(api): :fire: Remove `Id`, `Price`, `Discount` in Product Dto and move it to Variant Dto

--- src/Data/Dto/ProductDto.cs
+++ src/Data/Dto/ProductDto.cs
@@ -4,12 +4,9 @@ namespace The_Plague_Api.Data.Dto
 {
   public class ProductDto
   {
-    public string? Id { get; set; }
     public string Name { get; set; } = String.Empty;
     public string? Description { get; set; }
     public Image? Image { get; set; }
-    public decimal Price { get; set; }
-    public DiscountDto? Discount { get; set; }
     public List<VariantDto> Variants { get; set; } = new List<VariantDto>();
   }
 }
