185bd5564ac046e1925f3e000c9a2110e0d2ba30
Joshua Ramos <auhsojsomar@gmail.com>
2024-10-30 13:26:49 +0800

    ci(api): :card_file_box: Add `Id` in ColorDto to display the id upon fetching the products

--- src/Data/Dto/ColorDto.cs
+++ src/Data/Dto/ColorDto.cs
@@ -2,6 +2,7 @@ namespace The_Plague_Api.Data.Dto
 {
   public class ColorDto
   {
+    public string? Id { get; set; }
     public string Name { get; set; } = String.Empty;
     public string HexCode { get; set; } = String.Empty;
   }
