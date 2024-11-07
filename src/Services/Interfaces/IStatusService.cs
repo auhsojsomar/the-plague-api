7a13927bfc22fba155bdc44ca2631c5eee44429b
Joshua Ramos <auhsojsomar@gmail.com>
2024-11-08 01:19:01 +0800

    refactor: :truck: Rename from `Status` to `OrderStatus`

--- src/Services/Interfaces/IStatusService.cs
+++ src/Services/Interfaces/IStatusService.cs
@@ -2,12 +2,12 @@ using The_Plague_Api.Data.Entities.Order;
 
 namespace The_Plague_Api.Services.Interfaces
 {
-  public interface IStatusService
+  public interface IOrderStatusService
   {
-    Task<Status> CreateAsync(Status status);
-    Task<IEnumerable<Status>> GetAllAsync();
-    Task<Status?> GetByIdAsync(string id);
-    Task<bool> UpdateAsync(string id, Status status);
+    Task<OrderStatus> CreateAsync(OrderStatus orderStatus);
+    Task<IEnumerable<OrderStatus>> GetAllAsync();
+    Task<OrderStatus?> GetByIdAsync(string id);
+    Task<bool> UpdateAsync(string id, OrderStatus orderStatus);
     Task<bool> DeleteAsync(string id);
   }
 }
--- src/Services/StatusService.cs
+++ src/Services/StatusService.cs
@@ -4,31 +4,31 @@ using The_Plague_Api.Services.Interfaces;
 
 namespace The_Plague_Api.Services
 {
-  public class StatusService : IStatusService
+  public class OrderStatusService : IOrderStatusService
   {
-    private readonly IStatusRepository _statusRepository;
+    private readonly IOrderStatusRepository _statusRepository;
 
-    public StatusService(IStatusRepository statusRepository)
+    public OrderStatusService(IOrderStatusRepository statusRepository)
     {
       _statusRepository = statusRepository;
     }
 
-    public async Task<Status> CreateAsync(Status status)
+    public async Task<OrderStatus> CreateAsync(OrderStatus status)
     {
       return await _statusRepository.CreateAsync(status);
     }
 
-    public async Task<IEnumerable<Status>> GetAllAsync()
+    public async Task<IEnumerable<OrderStatus>> GetAllAsync()
     {
       return await _statusRepository.GetAllAsync();
     }
 
-    public async Task<Status?> GetByIdAsync(string id)
+    public async Task<OrderStatus?> GetByIdAsync(string id)
     {
       return await _statusRepository.GetByIdAsync(id);
     }
 
-    public async Task<bool> UpdateAsync(string id, Status status)
+    public async Task<bool> UpdateAsync(string id, OrderStatus status)
     {
       status.DateModified = DateTime.UtcNow;
       return await _statusRepository.UpdateAsync(id, status);
