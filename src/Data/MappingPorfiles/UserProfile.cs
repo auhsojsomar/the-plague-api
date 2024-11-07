2030f2e7c062d1d54b677590f5ef60689096ba44
Joshua Ramos <auhsojsomar@gmail.com>
2024-11-07 18:31:13 +0800

    feat(api): :building_construction: Map `UserDto` Fullname to `UserDto` Firstname, Middlename, Lastname

--- src/Data/MappingPorfiles/UserProfile.cs
+++ src/Data/MappingPorfiles/UserProfile.cs
@@ -1,6 +1,6 @@
 using AutoMapper;
 using The_Plague_Api.Data.Dto;
-using The_Plague_Api.Data.Entities;
+using The_Plague_Api.Data.Entities.User;
 
 namespace The_Plague_Api.Data.MappingPorfiles
 {
@@ -8,9 +8,12 @@ namespace The_Plague_Api.Data.MappingPorfiles
   {
     public UserProfile()
     {
-      CreateMap<UserEmailDto, User>()
-       .ForMember(dest => dest.Id, opt => opt.Ignore())
-       .ReverseMap();
+      CreateMap<UserDto, User>()
+       .ForMember(dest => dest.Id, opt => opt.Ignore());
+
+      CreateMap<User, UserDto>()
+        .ForMember(dest => dest.FullName, opt => opt.MapFrom(src =>
+          $"{src.FirstName} {src.MiddleName} {src.LastName}".Trim()));
 
       CreateMap<UserLoginDto, User>()
        .ForMember(dest => dest.Id, opt => opt.Ignore())
