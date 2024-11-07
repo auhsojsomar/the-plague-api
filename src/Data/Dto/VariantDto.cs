460bcb06b669c361d7487a0f78f276b34b290dae
Joshua Ramos <auhsojsomar@gmail.com>
2024-11-08 04:17:21 +0800

    refactor(api): :recycle: use the `CalculateSalePrice` helper method

--- src/Data/Dto/VariantDto.cs
+++ src/Data/Dto/VariantDto.cs
@@ -1,34 +1,26 @@
 using System.Text.Json.Serialization;
 using The_Plague_Api.Data.Entities.Product;
+using The_Plague_Api.Helpers;
 
 namespace The_Plague_Api.Data.Dto
 {
   public class VariantDto
   {
     public string? Id { get; set; }
+
     public SizeDto Size { get; set; } = new SizeDto();
+
     public ColorDto Color { get; set; } = new ColorDto();
+
     public decimal Price { get; set; }
+
     public int Quantity { get; set; }
 
-    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)] // Ignore when null
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public DiscountDto? Discount { get; set; }
 
-    // Read-only property to calculate SalePrice
-    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)] // Ignore when null
-    public decimal? SalePrice => GetSalePrice();
-
-    // Method to get SalePrice only if Discount exists
-    public decimal? GetSalePrice()
-    {
-      if (Discount == null) return null;
-
-      return Discount.Type switch
-      {
-        DiscountType.Percentage => Price - (Price * (Discount.Value / 100)),
-        DiscountType.FixedAmount => Price - Discount.Value,
-        _ => Price // Fallback to original Price if type is unknown
-      };
-    }
+    // Read-only property to calculate SalePrice using PriceHelper
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public decimal? SalePrice => PriceHelpers.CalculateSalePrice(Price, Discount?.Type, Discount?.Value);
   }
 }
