c535bf8a2c427513d73411d4644cca0d3301c85e
Joshua Ramos <auhsojsomar@gmail.com>
2024-11-07 18:49:32 +0800

    ci: :coffin: Remove unused `GetByName` Service

--- src/Repositories/Interfaces/IPaymentMethod.cs
+++ src/Repositories/Interfaces/IPaymentMethod.cs
@@ -4,7 +4,6 @@ public interface IPaymentMethodRepository
 {
   Task<IEnumerable<PaymentMethod>> GetAllAsync();
   Task<PaymentMethod?> GetByIdAsync(string id);
-  Task<PaymentMethod?> GetByNameAsync(string name);
   Task<PaymentMethod> CreateAsync(PaymentMethod paymentMethod);
   Task<bool> UpdateAsync(string id, PaymentMethod paymentMethod);
   Task<bool> DeleteAsync(string id);
