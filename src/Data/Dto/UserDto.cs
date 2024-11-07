a96524f9ea5008d487f30256bc4ac661de387ca6
Joshua Ramos <auhsojsomar@gmail.com>
2024-11-07 18:29:00 +0800

    refactor: :monocle_face: Rename `UserEmailDto` to `UserDto` and include `Fullname` and `Address` in UserDto

---
+++ src/Data/Dto/UserDto.cs
@@ -0,0 +1,15 @@
+using The_Plague_Api.Data.Entities.User;
+
+namespace The_Plague_Api.Data.Dto
+{
+  public class UserDto
+  {
+    public string? Id { get; set; }
+
+    public required string Email { get; set; }
+
+    public required string FullName { get; set; }
+
+    public required ICollection<Address> Address { get; set; }
+  }
+}
--- src/Data/Dto/UserEmailDto.cs
+++
@@ -1,8 +0,0 @@
-namespace The_Plague_Api.Data.Dto
-{
-  public class UserEmailDto
-  {
-    public string? Id { get; set; }
-    public string Email { get; set; } = String.Empty;
-  }
-}
