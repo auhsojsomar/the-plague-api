33d2e220dd602568aa63792de6282a3f1631bc94
Joshua Ramos <auhsojsomar@gmail.com>
2024-12-12 10:05:40 +0800

    build: :building_construction: Update development configuration for docker and mongodb settings

--- src/Settings/MongoDbSettings.cs
+++ src/Settings/MongoDbSettings.cs
@@ -3,9 +3,10 @@ namespace The_Plague_Api.Settings
   public class MongoDbSettings
   {
     public required string Host { get; set; }
+    public required string Port { get; set; }
     public required string Username { get; set; }
     public required string Password { get; set; }
     public required string DatabaseName { get; set; }
-    public string ConnectionString => $"mongodb+srv://{Username}:{Password}@{Host}/?retryWrites=true&w=majority&appName=the-plague-cluster";
+    public string ConnectionString => $"mongodb://{Username}:{Password}@{Host}:{Port}";
   }
 }
